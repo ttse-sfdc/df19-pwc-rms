109bbe8c9f673c28b4951b49539baaa3cc6b7f40
ttse-sfdc <ttse@salesforce.com>
2019-11-10 14:44:00 -0800

    Update ea.js

--- public/resources/scripts/ea.js
+++ public/resources/scripts/ea.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    var customDomain = 'df19-ea-keynote';
+    var predictionDefinition = '0OR1H000000Gma9WAC';
+    var accessToken = '00DB0000000YK2U!ARUAQII1xrw3c_WfvzvVHxaHm1mzoo9VtME62wN.lUj76VgqI6Q4zDNXhV_Mr_FzOfRHMMQr9f067k.6C5sAG87Y.uwcyZve';
+
     //add button characteristics
     $( "div.ea_update" ).hover(function() {
         $(this).css('cursor', 'pointer');
@@ -14,12 +18,17 @@ $(document).ready(function(){
         $( "div.ea_discovery_panel .text p span" ).html("BLAH BLAH BLAH");
 
         $.ajax({
+            beforeSend: function(xhr){
+                xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+                xhr.setRequestHeader('Authorization', 'OAuth ' + accessToken);
+                xhr.setRequestHeader('Accept', 'application/json');
+            },
             type: "POST",
             crossdomain: true,
-            url: "https://df19-ea-keynote.lightning.force.com/services/data/v46.0/smartdatadiscovery/predict",
+            url: "https://" + customDomain + ".my.salesforce.com/services/data/v46.0/smartdatadiscovery/predict",
             dataType: "json",
             data: { 
-                "predictionDefinition": "0OR1H000000Gma9WAC",
+                "predictionDefinition": predictionDefinition,
                 "type": "RawData",
                 "columnNames": ["StageName","CloseDate","Account.BillingCountry","IsClosed","IsWon"],
                 "rows": [
