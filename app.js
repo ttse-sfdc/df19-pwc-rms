830e21cde16048e0d6112218a041c7457b46d5a9
ttse-sfdc <ttse@salesforce.com>
2019-11-10 14:24:16 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,14 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//app.use(cors());
-
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
